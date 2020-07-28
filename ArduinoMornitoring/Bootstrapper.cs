484d1435a84c2007c81032abf120d9770fade7f8
bgu609 <giung609@naver.com>
2020-07-28 17:29:41 +0900

    2020-07-28 wpf

---
+++ ArduinoMornitoring/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using ArduinoMornitoring.ViewModels;
+using Caliburn.Micro;
+using System.Windows;
+
+namespace ArduinoMornitoring
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ ArduinoMornitoring/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Caliburn.Micro;
+
+namespace ArduinoMornitoring.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+            DisplayName = "Arduino Mornitoring App";
+        }
+    }
+}
